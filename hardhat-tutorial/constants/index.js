75ec1c77dc1aaabf63e7347b9d2ac14ffa4c7906
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-03-17 05:14:58 +0100

    updated:NftCollwction

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x43eF82e90BB243096169fCaF76B418369f4f8956";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
